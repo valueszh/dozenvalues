46aa7c1b953f4362fa5a0571a24f22b10202037e
valueszh <118618537+valueszh@users.noreply.github.com>
2023-09-22 08:12:40 +0800

    Update custom.js

--- dist/custom.js
+++ dist/custom.js
@@ -69,7 +69,12 @@ class TouchCanvas extends Canvas {
                 axisLabel = `${ui.resultstext.axis_name} ${ui.axes[ind].axisname}: ${tier}`;
             }
             else {
-                axisLabel = `${ui.axes[ind].axisname} ${ui.resultstext.axis_name}: ${tier}`;
+                if (ui.resultstext.axis_name_no_space) {
+                    axisLabel = `${ui.axes[ind].axisname} ${ui.resultstext.axis_name}: ${tier}`;
+                }
+                else {
+                    axisLabel = `${ui.axes[ind].axisname}${ui.resultstext.axis_name}：${tier}`;
+                }
             }
             this.drawBar(ind, colors, stat, axisLabel);
         });
